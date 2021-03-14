6375b2a395c9f97e88ba485eada4a92e6ce147d6
dolev146 <62290677+dolev146@users.noreply.github.com>
2021-03-14 15:14:40 +0200

    untill minute 42

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,36 @@
 const express = require("express");
-
+const Article = require("./../models/article");
 const router = express.Router();
 
-router.get('/new', (req, res) => {
-    res.render('articles/new')
-})
+router.get("/new", (req, res) => {
+  res.render("articles/new", { article: new Article() });
+});
+
+router.get("/:id",async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  if (article == null) res.redirect('/')
+  res.render("articles/show", { article });
+});
+
+router.post("/", async (req, res) => {
+  console.log(req.body.title);
+  console.log(req.body.description);
+  console.log(req.body.markdown);
+  let article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  });
+
+  try {
+    article = await article.save();
+    console.log(article)
+     console.log(article.id);
+    res.redirect(`/articles/${article.id}`);
+  } catch (error) {
+    console.log(error);
+    res.render("articles/new", { article: article });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require("express");
+const mongoose = require('mongoose')
 const articleRouter = require("./routes/articles");
+const Article = require('./models/article')
 const app = express();
 
+mongoose.connect("mongodb://localhost/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  // we're connected!
+  console.log("mongo connected")
+});
+
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: false}))
 
-app.use("/articles", articleRouter);
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test Article ",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-    {
-      title: "Test Article ",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-  ];
+
+app.get("/", async (req, res) => {
+ const articles = await Article.find().sort({createdAt : 'desc'})
   res.render("articles/index", { articles: articles });
 });
+
+app.use("/articles", articleRouter);
 
 app.listen(5000);
